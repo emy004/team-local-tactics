d5d08f98b780d44257dce018541c72b0c66d896c
erlan <emy004@uib.no>
2022-03-03 23:55:56 +0100

    Attempts to split the client side

--- client.py
+++ client.py
@@ -1,23 +1,35 @@
 from team_local_tactics import *
 from champlistloader import load_some_champs
-from socket import socket, AF_INET,SOCK_DGRAM
+from socket import socket, AF_INET,SOCK_DGRAM, create_connection
 import pickle
 
 sock=socket(AF_INET,SOCK_DGRAM)
-sock2=socket(AF_INET,SOCK_DGRAM)
-sock2.bind(('localhost',5556))
+sock.connect(("localhost",5555))
+
 champs=load_some_champs()
 print_available_champs(champs)
+player=[]
 player1=[]
 player2=[]
-for _ in range(2):
-        input_champion('Player 1', 'red', champs, player1, player2)
-        input_champion('Player 2', 'blue', champs, player2, player1)
+while len(player)!=2:
+    champ=input("Choose your Champion:"  )
+    if champ in player:
+        print ("You have already chosen that champion")
+    else:
+        player.append(champ)
+#for _ in range(2):
+ #       input_champion('Player 1', 'red', champs, player1, player2)
+  #      input_champion('Player 2', 'blue', champs, player2, player1)
+
+
+print (player)
 for i in range(2):
-    sock.sendto(player1[i].encode(),("localhost",5555))
-    sock.sendto(player2[i].encode(),("localhost",5555))
+    sock.sendto(player[i].encode(),(("Localhost",5555)))
+    #sock.sendto(player2[i].encode(),(("Localhost",5555)))
 
 
+sock2=socket(AF_INET,SOCK_DGRAM)
+sock2.bind(('localhost',5556))
 
 data,_=sock2.recvfrom(1024)
 match=pickle.loads(data)
--- server.py
+++ server.py
@@ -2,8 +2,9 @@ from team_local_tactics import *
 from rich import print
 from rich.prompt import Prompt
 from rich.table import Table
-from socket import socket, AF_INET,SOCK_DGRAM
+from socket import socket,AF_INET, SOCK_DGRAM
 from core import Champion, Match, Shape, Team
+from threading import Thread
 import pickle
 
 sock=socket(AF_INET,SOCK_DGRAM)
@@ -13,10 +14,28 @@ sock2=socket(AF_INET,SOCK_DGRAM)
 
 player1=[]
 player2=[]
+adresses=[]
 champs=load_some_champs()
-for i in range(2):
-    player1.append(sock.recvfrom(1024)[0].decode())
-    player2.append(sock.recvfrom(1024)[0].decode())
+
+while  len(player2)!=2:
+ data,adress=sock.recvfrom(1024)
+ if adress not in adresses:
+    adresses.append(adress)
+    player1.append(data.decode())
+    print (player1)
+ elif adress==adresses[0]:
+    player1.append(data.decode())
+    print (player1)
+ elif adress!=adress[0]:
+    adresses.append(adress)
+    player2.append(data.decode())
+    print (player2)
+
+            
+
+#for i in range(2):        
+    #player1.append(sock.recvfrom(1024)[0].decode())
+    #player2.append(sock.recvfrom(1024)[0].decode())
 
 print ("Player one chose:" + player1[0] + " and " + player1 [1])
 print ("Player two chose:" + player2 [0] + " and " + player2[1])
@@ -28,4 +47,5 @@ match = Match(
 match.play()
 
 sock2.sendto(pickle.dumps(match),(("localhost",5556)))
+
 
